88413d69a807995d760e3b7d56211c4e9a7ec974
Martin1505 <martintordal@gmail.com>
2021-05-02 14:02:55 +0200

    Header footer components

---
+++ src/components/FooterMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <footer class="grid-two-columns">
+            <section>
+              <h4>Kontakt oss</h4>
+              <p>Telefon: 41226594</p>
+              
+            </section>
+            <section>
+              <h4>Adresse</h4>
+              <p>Morra di veien 10</p>
+            </section>
+        </footer>
+    </section>
+</template>
---
+++ src/components/HeaderMain.vue
@@ -0,0 +1,36 @@
+<template>
+    <section>
+        <h1>Find a Musician
+        </h1>
+        <nav>
+            <ul>
+                <li><router-link to="/">Legg inn oppdrag</router-link></li>
+                <li><router-link to="/player-page">Våre eskorter</router-link></li>
+            </ul>
+        </nav>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "HeaderMain"
+}
+</script>
+
+<style scoped>
+
+ul {
+    width: 100%;
+    height: 1rem;
+    background-color: rgb(56, 56, 56);
+    justify-content: space-around;
+    align-items: center;
+}
+
+
+a {
+    text-decoration: none;
+    color: rgb(138, 138, 138);
+}
+
+</style>
